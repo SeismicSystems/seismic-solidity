ca98cf55feb58377d2d4f41a5daaa78f56b7e9d0
Rona Wang <rona@mit.edu>
2024-09-17 13:50:57 -0400

    Updated ExpressionCompiler with shielded integer

--- libsolidity/codegen/ExpressionCompiler.cpp
+++ libsolidity/codegen/ExpressionCompiler.cpp
@@ -1999,6 +1999,8 @@ bool ExpressionCompiler::visit(MemberAccess const& _memberAccess)
 			MagicType const* arg = dynamic_cast<MagicType const*>(_memberAccess.expression().annotation().type);
 			if (IntegerType const* integerType = dynamic_cast<IntegerType const*>(arg->typeArgument()))
 				m_context << (member == "min" ? integerType->min() : integerType->max());
+			else if (ShieldedIntegerType const* shieldedIntegerType = dynamic_cast<ShieldedIntegerType const*>(arg->typeArgument()))
+				m_context << (member == "min" ? shieldedIntegerType->min() : shieldedIntegerType->max());
 			else if (EnumType const* enumType = dynamic_cast<EnumType const*>(arg->typeArgument()))
 				m_context << (member == "min" ? enumType->minValue() : enumType->maxValue());
 			else
@@ -2475,6 +2477,8 @@ void ExpressionCompiler::appendCompareOperatorCode(Token _operator, Type const& 
 		solAssert(_type.sizeOnStack() == 1, "Comparison of multi-slot types.");
 		bool isSigned = false;
 		if (auto type = dynamic_cast<IntegerType const*>(&_type))
+			isSigned = type->isSigned();
+		else if (auto type = dynamic_cast<ShieldedIntegerType const*>(&_type))
 			isSigned = type->isSigned();
 
 		switch (_operator)
