f5f935ac817e6b40abdec229e56ace8a639a2b81
Rona Wang <rona@mit.edu>
2024-09-25 17:33:33 -0400

    fix bug on binary operations between suint and uint

--- libsolidity/ast/Types.cpp
+++ libsolidity/ast/Types.cpp
@@ -770,8 +770,11 @@ TypeResult IntegerType::binaryOperatorResult(Token _operator, Type const* _other
 		}
 		return this;
 	}
+	auto commonType = Type::commonType(this, _other);
+	if (_other->category() == Category::ShieldedInteger)
+		commonType = Type::commonType(dynamic_cast<ShieldedIntegerType const*>(this), dynamic_cast<ShieldedIntegerType const*>(_other));
 
-	auto commonType = Type::commonType(this, _other); //might be an integer or fixed point
+	 //might be an integer or fixed point
 	if (!commonType)
 		return nullptr;
 
