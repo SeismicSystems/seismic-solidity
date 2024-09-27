d85edebd4eb57921f7d90599276b423ac90240c5
Rona Wang <rona@mit.edu>
2024-09-27 11:49:57 -0400

    removed unused control flow

--- libsolidity/analysis/ConstantEvaluator.cpp
+++ libsolidity/analysis/ConstantEvaluator.cpp
@@ -242,13 +242,6 @@ std::optional<TypedRational> convertType(rational const& _value, Type const& _ty
 		else
 			return TypedRational{&_type, _value.numerator() / _value.denominator()};
 	}
-	else if (auto const* shieldedIntegerType = dynamic_cast<ShieldedIntegerType const*>(&_type))
-	{
-		if (_value > shieldedIntegerType->maxValue() || _value < shieldedIntegerType->minValue())
-			return std::nullopt;
-		else
-			return TypedRational{&_type, _value.numerator() / _value.denominator()};
-	}
 	else
 		return std::nullopt;
 }
