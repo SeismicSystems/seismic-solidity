70d2971610cc480586a0ef92ff2502d126466286
SFYLL <sd@seismic.systems>
2024-12-09 10:03:00 +0100

    more tests

---
+++ test/libsolidity/semanticTests/inheritance/shielded_deploy_cycle_with_transparent.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.8.0;
+
+contract SimpleContract {
+}
+
+contract Deployer {
+    address storedAddress;
+    SimpleContract private storedSimpleContract;
+    saddress storedSimpleContractCopy;
+
+    function setUp(address _storedAddress, saddress _simpleContract) public {
+        storedAddress = _storedAddress;
+        storedSimpleContract = SimpleContract(_simpleContract);
+        storedSimpleContractCopy = _simpleContract;
+    }
+
+    function StoreThenLoadThenCStore() public returns (address privateLoad) {
+        address publicLoad = publicWriteAndLoad();
+
+        require(storedAddress == publicLoad, "Couldn't switch private address to public");
+
+        privateLoad = privateWriteAndLoad();
+
+        require(address(storedSimpleContractCopy) == privateLoad, "Couldn't switch public address to private");
+    }
+
+    function publicWriteAndLoad() internal returns (address publicLoad) {
+        assembly {
+            sstore(1, sload(storedAddress.slot))
+        }
+
+        assembly {
+            publicLoad := sload(1)
+        }
+    }
+
+    function privateWriteAndLoad() internal returns (address privateLoad) {
+        assembly {
+            cstore(1, cload(storedSimpleContractCopy.slot))
+        }
+
+        assembly {
+            privateLoad := cload(1)
+        }
+    }
+}
+
+
+// ----
+// setUp(address,saddress): 0x1212121212121212121212121212120000000012, 0x1212121212121212121212121212120000000013
+// StoreThenLoadThenCStore() -> 0x1212121212121212121212121212120000000013
+
