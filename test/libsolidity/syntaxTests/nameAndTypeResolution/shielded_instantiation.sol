712d88e7177d3e8549c51a0a75b0d66ecff60c63
SFYLL <sd@seismic.systems>
2024-12-11 09:46:29 +0100

    tests for new logic

---
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/shielded_instantiation.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+contract SimpleContract {
+}
+
+contract InstantiationFail {
+    SimpleContract contracts;
+    function instantiate(saddress _contractAddress) public  {
+        contracts = SimpleContract(_contractAddress);
+    }
+}
+// ----
+// TypeError 7399: (234-266): Instantiating a contract with a saddress is not yet supported
