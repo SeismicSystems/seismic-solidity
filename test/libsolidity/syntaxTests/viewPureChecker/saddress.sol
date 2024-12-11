ab3329572931c705769557dba332fb82ee2b15b9
SFYLL <sd@seismic.systems>
2024-12-11 10:37:15 +0100

    upgrading tests

--- test/libsolidity/syntaxTests/viewPureChecker/saddress.sol
+++ test/libsolidity/syntaxTests/viewPureChecker/saddress.sol
@@ -1,21 +1,9 @@
 contract C {
-    function f() public view returns (uint) {
-        return saddress(this).balance;
-    }
-    function g() public view returns (uint) {
-        return saddress(0).balance;
-    }
     function h() public view returns (bytes32) {
         return saddress(0).codehash;
     }
-
-    function builtins() public {
-        payable(saddress(this)).transfer(1);
-        require(payable(saddress(this)).send(2));
-        (bool success,) = saddress(this).delegatecall("");
-        require(success);
-		(success,) = saddress(this).call("");
-        require(success);
+    function g() public view returns (bytes memory) {
+        return saddress(0).code;
     }
 }
 // ----
