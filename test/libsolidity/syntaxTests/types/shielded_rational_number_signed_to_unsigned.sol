fd812a9567f00a98538df32a512c288d7abc4b8b
SFYLL <sd@seismic.systems>
2024-12-19 13:31:49 +0100

    modify test

--- test/libsolidity/syntaxTests/types/shielded_rational_number_signed_to_unsigned.sol
+++ test/libsolidity/syntaxTests/types/shielded_rational_number_signed_to_unsigned.sol
@@ -5,5 +5,5 @@ contract c {
     }
 }
 // ----
-// TypeError 9574: (52-64): Type int_const -1 is not implicitly convertible to expected type suint256.
-// TypeError 9640: (84-93): Explicit type conversion not allowed from "int_const -1" to "suint256". Cannot implicitly convert signed literal to unsigned type.
+// TypeError 9640: (62-71): Explicit type conversion not allowed from "int_const -1" to "suint256". Cannot implicitly convert signed literal to unsigned type.
+// TypeError 9640: (91-100): Explicit type conversion not allowed from "int_const -1" to "suint256". Cannot implicitly convert signed literal to unsigned type.
