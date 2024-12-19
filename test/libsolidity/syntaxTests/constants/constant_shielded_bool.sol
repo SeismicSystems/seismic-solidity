3ad31a0202077818ee0d89cae7c377533ad4c6bf
SFYLL <sd@seismic.systems>
2024-12-19 13:32:16 +0100

    adding more tests

---
+++ test/libsolidity/syntaxTests/constants/constant_shielded_bool.sol
@@ -0,0 +1,5 @@
+contract C {
+    sbool constant b = true;
+}
+// ----
+// DeclarationError 7491: (17-40): Shielded objects cannot be set to constant.
---
+++ test/libsolidity/syntaxTests/parsing/shielded_conditional.sol
@@ -0,0 +1,27 @@
+contract TestSbool {
+    function f(sbool c) public returns (uint) {
+        if (c) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function g(sbool c) public returns (uint) {
+        return c ? 1 : 2;
+    }
+
+    function h() public returns (uint) {
+        sbool i = sbool(true);
+        for (; i; ) {
+            return 3;
+        }
+        return 4;
+    }
+}
+
+// ----
+// Warning 9661: (294-315): Bool Literals converted to shielded bools will leak during contract deployment.
+// Warning 2018: (25-162): Function state mutability can be restricted to pure
+// Warning 2018: (168-243): Function state mutability can be restricted to pure
+// Warning 2018: (249-394): Function state mutability can be restricted to pure
