8697581ffef5e4e33fed3a42bf6765063c42f2e9
SFYLL <sd@seismic.systems>
2024-12-23 10:21:23 +0100

    missing semantic tests

--- test/libsolidity/semanticTests/expressions/shielded_conditional_expression_different_types.sol
+++ test/libsolidity/semanticTests/expressions/shielded_conditional_expression_different_types.sol
@@ -1,11 +1,11 @@
 contract test {
-    function f(bool cond) public returns (uint) {
-        suint8 x = 0xcd;
-        suint16 y = 0xabab;
+    function f(sbool cond) public returns (uint) {
+        suint8 x = suint8(0xcd);
+        suint16 y = suint16(0xabab);
         return cond ? uint(x) : uint(y);
     }
 }
 // ----
-// f(bool): true -> 0xcd
-// f(bool): false -> 0xabab
+// f(sbool): true -> 0xcd
+// f(sbool): false -> 0xabab
 
