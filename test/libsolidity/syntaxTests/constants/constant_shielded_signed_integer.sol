1b82c265b7007e7fb0169da7088cc86b88752c42
Spencer Solit <sol@seismic.systems>
2025-04-15 12:22:09 -0400

    typo

--- test/libsolidity/syntaxTests/constants/constant_shielded_signed_integer.sol
+++ test/libsolidity/syntaxTests/constants/constant_shielded_signed_integer.sol
@@ -1,5 +1,5 @@
 contract C {
-    sint constant b2 = 3;
+    sint constant b = 3;
 }
 // ----
 // DeclarationError 7491: (17-37): Shielded objects cannot be set to constant or immutable.
--- test/libsolidity/syntaxTests/immutable/immutable_shielded_bool.sol
+++ test/libsolidity/syntaxTests/immutable/immutable_shielded_bool.sol
@@ -1,7 +1,5 @@
 contract C {
-    sbool immutable b2 = true;
+    sbool immutable b = true;
 }
- // ----
- // DeclarationError 7491: (17-40): Shielded objects cannot be set to constant or immutable.
 // ----
-// DeclarationError 7491: (17-42): Shielded objects cannot be set to constant or immutable.
+// DeclarationError 7491: (17-40): Shielded objects cannot be set to constant or immutable.
