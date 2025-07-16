8792a258967f439326bd1abe39167927541a0a8f
Matthias Wright <matthias.s.wright@gmail.com>
2025-07-16 15:03:34 -0400

    test: add syntax test for public variables

---
+++ test/libsolidity/syntaxTests/types/sbytes_public_variables.sol
@@ -0,0 +1,17 @@
+contract C {
+    // Test public sbytes variables - should generate getter functions
+    sbytes1 public sb1;
+    sbytes8 public sb8;
+    sbytes32 public sb32;
+    
+    constructor() {
+        sb1 = sbytes1(0x01);
+        sb8 = sbytes8(0x0102030405060708);
+        sb32 = sbytes32(0x0102030405060708091011121314151617181920212223242526272829303132);
+    }
+}
+
+// ----
+// TypeError 7091: (88-106): Shielded Types are not supported for public state variables.
+// TypeError 7091: (112-130): Shielded Types are not supported for public state variables.
+// TypeError 7091: (136-156): Shielded Types are not supported for public state variables.
