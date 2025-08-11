6863d75993c988e3810c5428374163653c590b0f
Matthias Wright <matthias.s.wright@gmail.com>
2025-08-12 01:15:30 +0800

    chore: use script to assign error code

--- libsolidity/analysis/TypeChecker.cpp
+++ libsolidity/analysis/TypeChecker.cpp
@@ -3497,7 +3497,7 @@ bool TypeChecker::visit(IndexAccess const& _access)
 			if (type(*index)->isShielded())
 			{
 				m_errorReporter.fatalTypeError(
-					0000_error,
+					5910_error,
 					index->location(),
 					"Shielded types are not allowed as array indices."
 				);
