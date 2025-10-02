8858396c51a4de680b0f51d43681e256f20848d4
Matthias Wright <matthias.s.wright@gmail.com>
2025-10-02 22:26:32 +0800

    test: update semantic tests

--- test/libsolidity/semanticTests/array/shielded_array_storage_push_empty_length_address.sol
+++ test/libsolidity/semanticTests/array/shielded_array_storage_push_empty_length_address.sol
@@ -1,6 +1,6 @@
 contract C {
     saddress[] addressArray;
-    function set_get_length(suint256 len) public returns (uint256)
+    function set_get_length(uint256 len) public returns (uint256)
     {
         while(addressArray.length < len)
             addressArray.push();
@@ -12,22 +12,22 @@ contract C {
 // ====
 // EVMVersion: >=Mercury
 // ----
-// set_get_length(suint256): 0 -> 0
-// set_get_length(suint256): 1 -> 1
-// set_get_length(suint256): 10 -> 10
-// set_get_length(suint256): 20 -> 20
-// set_get_length(suint256): 0 -> 0
+// set_get_length(uint256): 0 -> 0
+// set_get_length(uint256): 1 -> 1
+// set_get_length(uint256): 10 -> 10
+// set_get_length(uint256): 20 -> 20
+// set_get_length(uint256): 0 -> 0
 // gas irOptimized: 77628
 // gas legacy: 77730
 // gas legacyOptimized: 77162
-// set_get_length(suint256): 0xFF -> 0xFF
+// set_get_length(uint256): 0xFF -> 0xFF
 // gas irOptimized: 168565
 // gas legacy: 696850
 // gas legacyOptimized: 134488
-// set_get_length(suint256): 0xFFF -> 0xFFF
+// set_get_length(uint256): 0xFFF -> 0xFFF
 // gas irOptimized: 1908127
 // gas legacy: 9857362
 // gas legacyOptimized: 1393660
-// set_get_length(suint256): 0xFFFFF -> FAILURE # Out-of-gas #
+// set_get_length(uint256): 0xFFFFF -> FAILURE # Out-of-gas #
 // gas irOptimized: 100000000
 // gas legacyOptimized: 100000000
