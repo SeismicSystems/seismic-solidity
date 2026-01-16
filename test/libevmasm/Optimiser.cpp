f14bdcdc8bb387dda85bf0d0eb908f90049bc852
cdrappi <c@seismic.systems>
2026-01-16 12:32:12 -0500

    fix expected test behaviour

--- test/libevmasm/Optimiser.cpp
+++ test/libevmasm/Optimiser.cpp
@@ -2530,7 +2530,15 @@ BOOST_AUTO_TEST_CASE(cse_sstore_then_cload_no_optimization)
 		u256(0),
 		Instruction::CLOAD
 	};
-	checkCSE(input, input);
+	// Optimizer does stack manipulation but keeps CLOAD (doesn't replace with SSTORE value)
+	checkCSE(input, {
+		u256(0x42),
+		u256(0),
+		Instruction::SWAP1,
+		Instruction::DUP2,
+		Instruction::SSTORE,
+		Instruction::CLOAD
+	});
 }
 
 BOOST_AUTO_TEST_CASE(cse_cstore_then_sload_no_optimization)
@@ -2542,7 +2550,15 @@ BOOST_AUTO_TEST_CASE(cse_cstore_then_sload_no_optimization)
 		u256(0),
 		Instruction::SLOAD
 	};
-	checkCSE(input, input);
+	// Optimizer does stack manipulation but keeps SLOAD (doesn't replace with CSTORE value)
+	checkCSE(input, {
+		u256(0x42),
+		u256(0),
+		Instruction::SWAP1,
+		Instruction::DUP2,
+		Instruction::CSTORE,
+		Instruction::SLOAD
+	});
 }
 
 BOOST_AUTO_TEST_CASE(cse_sstore_cload_different_slots_no_optimization)
@@ -2578,11 +2594,13 @@ BOOST_AUTO_TEST_CASE(cse_sstore_sload_same_slot_can_optimize)
 		u256(0),
 		Instruction::SLOAD
 	};
+	// Optimizer eliminates SLOAD and keeps value on stack
 	checkCSE(input, {
 		u256(0x42),
 		u256(0),
-		Instruction::SSTORE,
-		Instruction::DUP1
+		Instruction::DUP2,
+		Instruction::SWAP1,
+		Instruction::SSTORE
 	});
 }
 
@@ -2595,11 +2613,13 @@ BOOST_AUTO_TEST_CASE(cse_cstore_cload_same_slot_can_optimize)
 		u256(0),
 		Instruction::CLOAD
 	};
+	// Optimizer eliminates CLOAD and keeps value on stack
 	checkCSE(input, {
 		u256(0x42),
 		u256(0),
-		Instruction::CSTORE,
-		Instruction::DUP1
+		Instruction::DUP2,
+		Instruction::SWAP1,
+		Instruction::CSTORE
 	});
 }
 
