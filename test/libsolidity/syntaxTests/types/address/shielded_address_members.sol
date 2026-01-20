e229fcf970d8380f7c3e0412eb75fc777ffea035
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2026-01-20 16:47:17 -0500

    test: add tests for shielded types return visibility
    Tests are currently failing since right now we only restrict returning shielded addresses and integers.

--- test/libsolidity/syntaxTests/types/address/shielded_address_members.sol
+++ test/libsolidity/syntaxTests/types/address/shielded_address_members.sol
@@ -1,9 +1,9 @@
 contract C {
-    function f() public view returns (saddress) { return address(this); }
+    function f() public view returns (saddress) { return saddress(this); }
     function g() public view returns (uint) { return f().balance; }
     function h() public view returns (bytes memory) { return f().code; }
     function i() public view returns (uint) { return f().code.length; }
     function j() public view returns (uint) { return h().length; }
 }
 // ----
-// DeclarationError 7492: (51-59): Shielded objects cannot be returned from public or external functions. Use internal or private functions or cast to an unshielded type.
+// TypeError 7492: (51-59): Shielded objects cannot be returned from public or external functions. Use internal or private functions or cast to an unshielded type.
