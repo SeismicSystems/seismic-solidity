ca5d568228564b26d831dade85589d77e305c397
cdrappi <c@seismic.systems>
2026-01-22 14:48:58 -0500

    test: update tests that check disallowing implicit conversion of saddress <=> address

--- test/libsolidity/syntaxTests/types/address/saddress_array_to_address_array_external_call.sol
+++ test/libsolidity/syntaxTests/types/address/saddress_array_to_address_array_external_call.sol
@@ -4,8 +4,12 @@ contract Helper {
     function returnsAddressArray() external pure returns (address[] memory) {
         return new address[](5);
     }
-    function returnsSaddressArray() external pure returns (saddress[] memory) {
+    function returnsSaddressArray() internal pure returns (saddress[] memory) {
         return new saddress[](5);
+    }
+    // External wrapper to test implicit conversion of return value
+    function testReturnsSaddressArrayConversion() external pure {
+        address[] memory a = returnsSaddressArray();
     }
 }
 
@@ -23,11 +27,10 @@ contract C {
     function testExternalCallReturnAssignment() external view {
         // Assignment from external call with wrong return type
         saddress[] memory sa = helper.returnsAddressArray();
-        address[] memory a = helper.returnsSaddressArray();
     }
 }
 // ----
-// TypeError 9553: (655-657): Invalid type for argument in function call. Invalid implicit conversion from saddress[] memory to address[] memory requested.
-// TypeError 9553: (694-695): Invalid type for argument in function call. Invalid implicit conversion from address[] memory to saddress[] memory requested.
-// TypeError 9574: (841-892): Type address[] memory is not implicitly convertible to expected type saddress[] memory.
-// TypeError 9574: (902-952): Type saddress[] memory is not implicitly convertible to expected type address[] memory.
+// TypeError 9574: (539-582): Type saddress[] memory is not implicitly convertible to expected type address[] memory.
+// TypeError 9553: (848-850): Invalid type for argument in function call. Invalid implicit conversion from saddress[] memory to address[] memory requested.
+// TypeError 9553: (887-888): Invalid type for argument in function call. Invalid implicit conversion from address[] memory to saddress[] memory requested.
+// TypeError 9574: (1034-1085): Type address[] memory is not implicitly convertible to expected type saddress[] memory.
