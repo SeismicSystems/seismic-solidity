90648ad6e54509a043cf5c1bd02014ed1ee80cda
Christian Drappi <christiandrappi@gmail.com>
2026-01-26 16:45:26 -0500

    test: add yul, syntax & semantic tests for timestamp_seconds/timestamp_ms

---
+++ test/libsolidity/syntaxTests/types/magic_block_timestamp_ms.sol
@@ -0,0 +1,12 @@
+contract C {
+    function f() public view returns (uint) {
+        return block.timestamp;
+    }
+    function g() public view returns (uint) {
+        return block.timestamp_ms;
+    }
+    function h() public view returns (uint) {
+        return block.timestamp_seconds;
+    }
+}
+// ----
