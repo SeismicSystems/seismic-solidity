d6b25846e8292244f96b40e12d2464e13bf36a7f
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2026-01-27 15:13:53 -0500

    test: fix cstore_then_sstore_should_fail sem test

--- test/libsolidity/semanticTests/inlineAssembly/cstore_then_sstore_should_fail.sol
+++ test/libsolidity/semanticTests/inlineAssembly/cstore_then_sstore_should_fail.sol
@@ -2,6 +2,12 @@ contract C {
     function test() external {
         assembly {
             cstore(0, 1)
+        }
+        // We put the following in a separate assembly block since the TypeChecker prevents
+        // mixing sstore and cstore in the same block. Eventually our typechecker should get smarter
+        // and also prevent mixing them across blocks when they access the same slot, but for now we
+        // at least make sure that SSTORE results in runtime failure.
+        assembly {
             sstore(0, 0x1337)
         }
     }
