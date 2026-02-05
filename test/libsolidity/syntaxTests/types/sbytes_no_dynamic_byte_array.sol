7043b72728e7e3aaf99f8a6f14a5773cc934da88
Christian Drappi <christiandrappi@gmail.com>
2026-02-05 14:58:14 -0500

    test(codegen): add syntax test for byte array helpers with non-shielded types
    Verify that bytes/string storage operations (push, pop, resize, assign)
    compile correctly. These operations use byte array helpers that should
    only emit sload/sstore since there is no shielded dynamic bytes type
    (sbytesN is a fixed-size value type, not a dynamic byte array).

---
+++ test/libsolidity/syntaxTests/types/sbytes_no_dynamic_byte_array.sol
@@ -0,0 +1,33 @@
+// Byte array helpers (resize, push, pop, transitLongToShort) only apply to
+// bytes/string. There is no shielded dynamic bytes type. sbytesN is a
+// fixed-size value type that does not route through byte array helpers.
+contract C {
+    bytes public b;
+    string public s;
+    sbytes32 sb;
+
+    // bytes/string use byte array storage helpers (sload/sstore only)
+    function testBytesPushPop() public {
+        b.push(0x01);
+        b.push(0x02);
+        b.pop();
+    }
+
+    function testBytesResize() public {
+        b = new bytes(64);
+        b = new bytes(16);
+        b = new bytes(0);
+    }
+
+    function testStringAssign() public {
+        s = "short";
+        s = "this is a long string that exceeds thirty two bytes in length!!";
+        s = "";
+    }
+
+    // sbytesN is a fixed-size value type, not a dynamic byte array
+    function testSbytesN() public {
+        sb = sbytes32(0x0102030405060708091011121314151617181920212223242526272829303132);
+    }
+}
+// ----
